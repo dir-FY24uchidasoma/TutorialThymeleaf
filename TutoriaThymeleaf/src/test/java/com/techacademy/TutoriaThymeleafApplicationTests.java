801b1f6df4c6d989aec442fc45c724db80245ebd
dir-FY24uchidasoma <SomaUchida()@Dirbato-1388x>
2024-04-24 16:33:14 +0900

    Lesson20

---
+++ TutoriaThymeleaf/src/test/java/com/techacademy/TutoriaThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techacademy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutoriaThymeleafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
